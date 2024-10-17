36031b917e9102e2f626f47719b197514bf03b2b
Rahul Singh <angad14723@gmail.com>
2024-10-17 12:50:05 +0530

    Initial Commit

---
+++ users/usersApp.swift
@@ -0,0 +1,17 @@
+//
+//  usersApp.swift
+//  users
+//
+//  Created by Angad on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct usersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ usersTests/usersTests.swift
@@ -0,0 +1,16 @@
+//
+//  usersTests.swift
+//  usersTests
+//
+//  Created by Angad on 17/10/24.
+//
+
+import Testing
+
+struct usersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
